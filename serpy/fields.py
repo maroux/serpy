8a7697ca426332e96a1896c00405fc7bb275e3bd
Aniruddha Maru <aniruddha@automatic.com>
2015-06-03 12:11:52 -0700

    Add support for deserialization
    - Added support for read-only fields
    - Renamed `.data` to `.representation` (and added `.internal_value`) for better clarity
    - Renamed `.to_value` to `.to_representation` (and added `to_internal_value`) for better clarity
    
    TBD update documentation, fix benchmarks, update benchmarks
    
    Fixes clarkduvall/serpy#1

--- serpy/fields.py
+++ serpy/fields.py
@@ -1,5 +1,6 @@
 import six
 import types
+import warnings
 
 
 class Field(object):
@@ -7,8 +8,8 @@ class Field(object):
 
     A :class:`Field` maps a property or function on an object to a value in the
     serialized result. Subclass this to make custom fields. For most simple
-    cases, overriding :meth:`Field.to_value` should give enough flexibility. If
-    more control is needed, override :meth:`Field.as_getter`.
+    cases, overriding :meth:`Field.to_representation` should give enough
+    flexibility. If more control is needed, override :meth:`Field.as_getter`.
 
     :param str attr: The attribute to get on the object, using the same format
         as ``operator.attrgetter``. If this is not supplied, the name this
@@ -16,38 +17,82 @@ class Field(object):
     :param bool call: Whether the value should be called after it is retrieved
         from the object. Useful if an object has a method to be serialized.
     :param bool required: Whether the field is required. If set to ``False``,
-        :meth:`Field.to_value` will not be called if the value is ``None``.
+        :meth:`Field.to_representation` will not be called if the value is
+        ``None``.
+    :param bool read_only: Whether the field is read-only. If set to ``False``,
+        the field won't be deserialized. If ``call`` is True, or if ``attr``
+        contains a '.', then this param is set to True.
     """
     #: Set to ``True`` if the value function returned from
     #: :meth:`Field.as_getter` requires the serializer to be passed in as the
     #: first argument. Otherwise, the object will be the only parameter.
     getter_takes_serializer = False
 
-    def __init__(self, attr=None, call=False, required=True):
+    #: Set to ``True`` if the value function returned from
+    #: :meth:`Field.as_setter` requires the serializer to be passed in as the
+    #: first argument. Otherwise, the object will be the only parameter.
+    setter_takes_serializer = False
+
+    def __init__(self, attr=None, call=False, required=True, read_only=False):
         self.attr = attr
         self.call = call
         self.required = required
+        self.read_only = read_only or call or \
+            (attr is not None and '.' in attr)
 
-    def to_value(self, value):
+    def to_representation(self, value):
         """Transform the serialized value.
 
         Override this method to clean and validate values serialized by this
         field. For example to implement an ``int`` field: ::
 
-            def to_value(self, value):
+            def to_representation(self, value):
                 return int(value)
 
         :param value: The value fetched from the object being serialized.
         """
         return value
-    to_value._serpy_base_implementation = True
+    to_representation._serpy_base_implementation = True
 
-    def _is_to_value_overriden(self):
-        to_value = self.to_value
-        # If to_value isn't a method, it must have been overriden.
-        if not isinstance(to_value, types.MethodType):
+    def _is_to_representation_overridden(self):
+        to_representation = self.to_representation
+        # If to_representation isn't a method, it must have been overridden.
+        if not isinstance(to_representation, types.MethodType):
             return True
-        return not getattr(to_value, '_serpy_base_implementation', False)
+        return not getattr(to_representation,
+                           '_serpy_base_implementation',
+                           False)
+
+    def to_value(self, obj):
+        warnings.warn(
+            ".to_value method is deprecated, use .to_representation instead",
+            DeprecationWarning,
+            stacklevel=2
+        )
+        return self.to_representation(obj)
+
+    def to_internal_value(self, data):
+        """Transform the serialized value into Python object
+
+        Override this method to clean and validate values deserialized by this
+        field. For example to implement an ``int`` field: ::
+
+            def to_internal_value(self, data):
+                return data
+
+        :param data: The data fetched from the object being deserialized.
+        """
+        return data
+    to_internal_value._serpy_base_implementation = True
+
+    def _is_to_internal_value_overridden(self):
+        to_internal_value = self.to_internal_value
+        # If to_internal_value isn't a method, it must have been overridden.
+        if not isinstance(to_internal_value, types.MethodType):
+            return True
+        return not getattr(to_internal_value,
+                           '_serpy_base_implementation',
+                           False)
 
     def as_getter(self, serializer_field_name, serializer_cls):
         """Returns a function that fetches an attribute from an object.
@@ -59,7 +104,7 @@ class Field(object):
         converted into a getter function using this method. During
         serialization, each getter will be called with the object being
         serialized, and the return value will be passed through
-        :meth:`Field.to_value`.
+        :meth:`Field.to_representation`.
 
         If a :class:`Field` has ``getter_takes_serializer = True``, then the
         getter returned from this method will be called with the
@@ -72,25 +117,52 @@ class Field(object):
         """
         return None
 
+    def as_setter(self, serializer_field_name, serializer_cls):
+        """Returns a function that sets an attribute on an object
+
+        Return ``None`` to use the default setter for the serializer defined in
+        :attr:`Serializer.default_setter`.
+
+        When a :class:`Serializer` is defined, each :class:`Field` will be
+        converted into a setter function using this method. During
+        deserialization, each setter will be called with the object being
+        deserialized with the argument passed as the return value of
+        :meth:`Field.to_internal_value`.
+
+        If a :class:`Field` has ``setter_takes_serializer = True``, then the
+        setter returned from this method will be called with the
+        :class:`Serializer` instance as the first argument, and the object
+        being serialized as the second.
+
+        :param str serializer_field_name: The name this field was assigned to
+            on the serializer.
+        :param serializer_cls: The :class:`Serializer` this field is a part of.
+        """
+        return None
+
 
 class StrField(Field):
     """A :class:`Field` that converts the value to a string."""
-    to_value = staticmethod(six.text_type)
+    to_representation = staticmethod(six.text_type)
+    to_internal_value = staticmethod(six.text_type)
 
 
 class IntField(Field):
     """A :class:`Field` that converts the value to an integer."""
-    to_value = staticmethod(int)
+    to_representation = staticmethod(int)
+    to_internal_value = staticmethod(int)
 
 
 class FloatField(Field):
     """A :class:`Field` that converts the value to a float."""
-    to_value = staticmethod(float)
+    to_representation = staticmethod(float)
+    to_internal_value = staticmethod(float)
 
 
 class BoolField(Field):
     """A :class:`Field` that converts the value to a boolean."""
-    to_value = staticmethod(bool)
+    to_representation = staticmethod(bool)
+    to_internal_value = staticmethod(bool)
 
 
 class MethodField(Field):
@@ -110,20 +182,28 @@ class MethodField(Field):
                 return foo_obj.bar - foo_obj.baz
 
         foo = Foo(bar=5, baz=10)
-        FooSerializer(foo).data
+        FooSerializer(foo).representation
         # {'plus': 15, 'minus': -5}
 
     :param str method: The method on the serializer to call. Defaults to
         ``'get_<field name>'``.
     """
     getter_takes_serializer = True
+    setter_takes_serializer = True
 
-    def __init__(self, method=None, **kwargs):
+    def __init__(self, getter=None, setter=None, **kwargs):
         super(MethodField, self).__init__(**kwargs)
-        self.method = method
+        self.getter_method = getter
+        self.setter_method = setter
 
     def as_getter(self, serializer_field_name, serializer_cls):
-        method_name = self.method
+        method_name = self.getter_method
         if method_name is None:
             method_name = 'get_{0}'.format(serializer_field_name)
-        return getattr(serializer_cls, method_name)
+        return getattr(serializer_cls, method_name, None)
+
+    def as_setter(self, serializer_field_name, serializer_cls):
+        method_name = self.setter_method
+        if method_name is None:
+            method_name = 'set_{0}'.format(serializer_field_name)
+        return getattr(serializer_cls, method_name, None)
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,34 +1,53 @@
-from .obj import Obj
+import unittest
+import warnings
+
 from serpy.fields import Field, MethodField, IntField, FloatField, StrField
 from serpy.serializer import Serializer, DictSerializer
-import unittest
+from tests.obj import Obj
 
 
 class TestSerializer(unittest.TestCase):
 
     def test_simple(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         a = Obj(a=5)
-        self.assertEqual(ASerializer(a).data['a'], 5)
+        self.assertEqual(ASerializer(a).representation['a'], 5)
 
-    def test_data_cached(self):
+        a = ASerializer(data={'a': 5}).internal_value
+        self.assertEqual(a.a, 5)
+
+    def test_data_and_obj_cached(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         a = Obj(a=5)
         serializer = ASerializer(a)
-        data1 = serializer.data
-        data2 = serializer.data
+        data1 = serializer.representation
+        data2 = serializer.representation
         # Use assertTrue instead of assertIs for python 2.6.
         self.assertTrue(data1 is data2)
 
+        serializer = ASerializer(data={'a': 5})
+        obj1 = serializer.internal_value
+        obj2 = serializer.internal_value
+        # Use assertTrue instead of assertIs for python 2.6.
+        self.assertTrue(obj1 is obj2)
+
     def test_inheritance(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         class CSerializer(Serializer):
+            _cls = Obj
+
             c = Field()
 
         class ABSerializer(ASerializer):
@@ -38,21 +57,34 @@ class TestSerializer(unittest.TestCase):
             pass
 
         a = Obj(a=5, b='hello', c=100)
-        self.assertEqual(ASerializer(a).data['a'], 5)
-        data = ABSerializer(a).data
+        self.assertEqual(ASerializer(a).representation['a'], 5)
+        data = ABSerializer(a).representation
         self.assertEqual(data['a'], 5)
         self.assertEqual(data['b'], 'hello')
-        data = ABCSerializer(a).data
+        data = ABCSerializer(a).representation
         self.assertEqual(data['a'], 5)
         self.assertEqual(data['b'], 'hello')
         self.assertEqual(data['c'], 100)
 
+        a = {'a': 5, 'b': 'hello', 'c': 100}
+        serializer = ASerializer(data=a)
+        self.assertEqual(serializer.internal_value.a, 5)
+        serializer = ABSerializer(data=a)
+        self.assertEqual(serializer.internal_value.a, 5)
+        self.assertEqual(serializer.internal_value.b, 'hello')
+        serializer = ABCSerializer(data=a)
+        self.assertEqual(serializer.internal_value.a, 5)
+        self.assertEqual(serializer.internal_value.b, 'hello')
+        self.assertEqual(serializer.internal_value.c, 100)
+
     def test_many(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         objs = [Obj(a=i) for i in range(5)]
-        data = ASerializer(objs, many=True).data
+        data = ASerializer(objs, many=True).representation
         self.assertEqual(len(data), 5)
         self.assertEqual(data[0]['a'], 0)
         self.assertEqual(data[1]['a'], 1)
@@ -60,116 +92,242 @@ class TestSerializer(unittest.TestCase):
         self.assertEqual(data[3]['a'], 3)
         self.assertEqual(data[4]['a'], 4)
 
+        data = [{'a': 0}, {'a': 1}, {'a': 2}, {'a': 3}, {'a': 4}]
+        objs = ASerializer(data=data, many=True).internal_value
+        self.assertEqual(len(objs), 5)
+        self.assertEqual(objs[0].a, 0)
+        self.assertEqual(objs[1].a, 1)
+        self.assertEqual(objs[2].a, 2)
+        self.assertEqual(objs[3].a, 3)
+        self.assertEqual(objs[4].a, 4)
+
     def test_serializer_as_field(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         class BSerializer(Serializer):
+            _cls = Obj
+
             b = ASerializer()
 
         b = Obj(b=Obj(a=3))
-        self.assertEqual(BSerializer(b).data['b']['a'], 3)
+        self.assertEqual(BSerializer(b).representation['b']['a'], 3)
+
+        data = {'b': {'a': 3}}
+        obj = BSerializer(data=data).internal_value
+        self.assertEqual(obj.b.a, 3)
 
     def test_serializer_as_field_many(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         class BSerializer(Serializer):
+            _cls = Obj
+
             b = ASerializer(many=True)
 
         b = Obj(b=[Obj(a=i) for i in range(3)])
-        b_data = BSerializer(b).data['b']
+        b_data = BSerializer(b).representation['b']
         self.assertEqual(len(b_data), 3)
         self.assertEqual(b_data[0]['a'], 0)
         self.assertEqual(b_data[1]['a'], 1)
         self.assertEqual(b_data[2]['a'], 2)
 
+        data = {'b': [{'a': 0}, {'a': 1}, {'a': 2}]}
+        obj = BSerializer(data=data).internal_value
+        self.assertEqual(len(obj.b), 3)
+        self.assertEqual(obj.b[0].a, 0)
+        self.assertEqual(obj.b[1].a, 1)
+        self.assertEqual(obj.b[2].a, 2)
+
     def test_serializer_as_field_call(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field()
 
         class BSerializer(Serializer):
+            _cls = Obj
+
             b = ASerializer(call=True)
 
         b = Obj(b=lambda: Obj(a=3))
-        self.assertEqual(BSerializer(b).data['b']['a'], 3)
+        self.assertEqual(BSerializer(b).representation['b']['a'], 3)
+        # TBD check that deserialization doesnt work
 
     def test_serializer_method_field(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = MethodField()
-            b = MethodField('add_9')
+            b = MethodField('add_9', 'sub_9')
 
             def get_a(self, obj):
                 return obj.a + 5
 
-            def add_9(self, obj):
-                return obj.a + 9
+            def set_a(self, obj, value):
+                obj.a = value - 5
 
-        a = Obj(a=2)
-        data = ASerializer(a).data
+            def add_9(self, obj):
+                return obj.b + 9
+
+            def sub_9(self, obj, value):
+                obj.b = value - 9
+
+        a = Obj(a=2, b=2)
+        data = ASerializer(a).representation
         self.assertEqual(data['a'], 7)
         self.assertEqual(data['b'], 11)
+        data = {'a': 7, 'b': 11}
+        obj = ASerializer(data=data).internal_value
+        self.assertEqual(obj.a, 2)
+        self.assertEqual(obj.b, 2)
 
-    def test_to_value_called(self):
+    def test_field_called(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = IntField()
             b = FloatField(call=True)
             c = StrField(attr='foo.bar.baz')
 
         o = Obj(a='5', b=lambda: '6.2', foo=Obj(bar=Obj(baz=10)))
-        data = ASerializer(o).data
+        data = ASerializer(o).representation
         self.assertEqual(data['a'], 5)
         self.assertEqual(data['b'], 6.2)
         self.assertEqual(data['c'], '10')
+        data = {'a': 5, 'b': 6.2, 'c': '10'}
+        obj = ASerializer(data=data).internal_value
+        self.assertEqual(obj.a, 5)
+        self.assertFalse(hasattr(obj, 'b'))
+        self.assertFalse(hasattr(obj, 'foo'))
 
     def test_dict_serializer(self):
         class ASerializer(DictSerializer):
+            _cls = Obj
+
             a = IntField()
             b = Field(attr='foo')
 
         d = {'a': '2', 'foo': 'hello'}
-        data = ASerializer(d).data
+        data = ASerializer(d).representation
         self.assertEqual(data['a'], 2)
         self.assertEqual(data['b'], 'hello')
+        data = {'a': 2, 'b': 'hello'}
+        obj = ASerializer(data=data).internal_value
+        self.assertEqual(obj.a, 2)
+        self.assertEqual(obj.foo, 'hello')
 
     def test_dotted_attr(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Field('a.b.c')
 
         o = Obj(a=Obj(b=Obj(c=2)))
-        data = ASerializer(o).data
+        data = ASerializer(o).representation
         self.assertEqual(data['a'], 2)
+        data = {'a': 2}
+        obj = ASerializer(data=data).internal_value
+        self.assertFalse(hasattr(obj, 'a'))
 
     def test_custom_field(self):
         class Add5Field(Field):
-            def to_value(self, value):
+            def to_representation(self, value):
                 return value + 5
 
+            def to_internal_value(self, data):
+                return data - 5
+
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = Add5Field()
 
         o = Obj(a=10)
-        data = ASerializer(o).data
+        data = ASerializer(o).representation
         self.assertEqual(data['a'], 15)
+        data = {'a': 15}
+        obj = ASerializer(data=data).internal_value
+        self.assertEqual(obj.a, 10)
 
     def test_optional_field(self):
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = IntField(required=False)
 
         o = Obj(a=None)
-        data = ASerializer(o).data
-        self.assertEqual(data['a'], None)
+        data = ASerializer(o).representation
+        self.assertIsNone(data['a'])
+
+        data = {'a': None}
+        obj = ASerializer(data=data).internal_value
+        self.assertIsNone(obj.a)
 
         o = Obj(a='5')
-        data = ASerializer(o).data
+        data = ASerializer(o).representation
         self.assertEqual(data['a'], 5)
 
+        data = {'a': 5}
+        obj = ASerializer(data=data).internal_value
+        self.assertEqual(obj.a, 5)
+
         class ASerializer(Serializer):
+            _cls = Obj
+
             a = IntField()
 
         o = Obj(a=None)
-        self.assertRaises(TypeError, lambda: ASerializer(o).data)
+        self.assertRaises(TypeError, lambda: ASerializer(o).representation)
 
+        data = {}
+        self.assertRaises(KeyError,
+                          lambda: ASerializer(data=data).internal_value)
+
+    def test_read_only_field(self):
+        class ASerializer(Serializer):
+            _cls = Obj
+
+            a = IntField(read_only=True)
+
+        o = Obj(a='5')
+        data = ASerializer(o).representation
+        self.assertEqual(data['a'], 5)
+
+        data = {'a': 5}
+        obj = ASerializer(data=data).internal_value
+        self.assertFalse(hasattr(obj, 'a'))
+
+    def test_cls_required_for_deserialization(self):
+        class ASerializer(Serializer):
+            a = IntField()
+
+        data = {'a': 5}
+        self.assertRaises(AttributeError,
+                          lambda: ASerializer(data=data).internal_value)
+
+    def test_data_backwards_compatibility(self):
+        class ASerializer(Serializer):
+            a = IntField()
+
+        o = Obj(a='5')
+        serializer = ASerializer(o)
+        self.assertIs(serializer.data, serializer.representation)
+
+    def test_data_deprecation_warning(self):
+        o = Obj(a='5')
+        serializer = Serializer(o)
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always', DeprecationWarning)
+            serializer.data
+            self.assertEqual(len(w), 1)
+            self.assertTrue(issubclass(w[-1].category, DeprecationWarning))
+            self.assertIn('deprecated', str(w[-1].message))
 
 if __name__ == '__main__':
     unittest.main()
